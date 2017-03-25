f18589db605a6e606cc990cdb502b24743c5e0d4
gaurav7goyal <gaurav1997dlp@gmail.com>
2017-03-26 01:34:48 +0530

    ghoda ji

---
+++ PDF/index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<!--
+Template Name: Academic Education V2
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html>
+<head>
+<title>Placement Drive Solution</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+ <!--SLIDER CSS CLASES -->
+ <link href="assets/Slides-SlidesJS-3/examples/playing/css/slider.css" rel="stylesheet" />
+    <!--CUSTOM STYLE -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- calander js-->
+    <link href="cala.css" rel="stylesheet">
+  <script type="text/javascript" src="reoccurringEventsCal.js"></script>
+</head>
+<body id="top">
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row0">
+  <div id="topbar" class="clear"> 
+    <!-- ################################################################################################ -->
+    <nav>
+      <ul>
+        <li style="font-size:130%"><a href="http://www.poornima.org">Home</a></li>
+        <li style="font-size:130%"><a href="#"> Login</a></li>
+        <li style="font-size:130%"><a href="internship.php">Internship</a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ --> 
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="clear"> 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.html">Computer department</a></h1>
+      <p>traning&placement </p>
+    </div>
+    <div class="fl_right">
+      <form class="clear" method="post" action="#">
+        <fieldset>
+          <legend>Search:</legend>
+          <input type="text" value="" placeholder="Search Here">
+          <button class="fa fa-search" type="submit" title="Search"><em>Search</em></button>
+        </fieldset>
+      </form>
+    </div>
+    <!-- ################################################################################################ --> 
+  </header>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row2">
+  <div class="rounded">
+    <nav id="mainav" class="clear"> 
+      <!-- ################################################################################################ -->
+      <ul class="clear">
+        <li class="active"><a href="index.html">Home</a></li>
+         <li><a href="aboutus/ABOUTUS.php">about us</a></li>
+           <li><a class="drop" href="#">Acadmic</a>
+        <ul>
+          <li><a href="#">Planning</a></li>
+           <li><a href="#">Syllabus</a></li>
+        </ul>
+        <li><a class="drop" href="#">recruiters</a>
+          <ul>
+            <li><a href="../company/gallery.php">Gallery</a></li>
+          <li><a href="company/past.php">up coming <br>recuruiters</a></li>
+            <li><a href="company/future.php"> past <br>recuruiters</a></li>
+            <li><a href="company/present.php"> present</a></li>
+          </ul>
+        </li>
+        <li><a class="drop" href="#">placement</a>
+          <ul>
+          <li><a href="placement/#">Details</a></li>
+            <li><a href="placement/#">Current</a></li>
+            <li><a class="drop" href="#">Before</a>
+              <ul>
+                <li><a href="placement/#">2015-16</a></li>
+                <li><a href="placement/#">2014-15</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+       
+        
+        <li><a href="#">contact us</a></li>
+       
+      
+      <!-- ################################################################################################ --> 
+    </nav>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper">
+  <div id="slider">
+    <div id="slide-wrapper" class="rounded clear"> 
+      <!-- ################################################################################################ -->
+       <!-- SLIDER SECTION -->
+    <div id="slides">
+        <img src="assets/img/slider/images/1.jpg" alt="" />
+        <img src="assets/img/slider/images/3.jpg" alt="" />
+        <img src="assets/img/slider/images/4.jpg" alt="" />
+        <img src="assets/img/slider/images/1.jpg" alt="" />
+
+    </div>
+   
+
+<!-- Marquee Text by Way2Tutorial.com -->
+
+    <!-- END SLIDER SECTION -->
+      <!-- ################################################################################################ -->
+     
+      <!-- ################################################################################################ --> 
+    </div>
+    <div class="rounded clear">
+ <marquee behavior="scroll" direction="left"scrollamount="15" ><img src="images/demo/gallery/1.png"><img src="images/demo/gallery/2.png"><img src="images/demo/gallery/3.png"><img src="images/demo/gallery/4.png"><img src="images/demo/gallery/5.png"><img src="images/demo/gallery/6.png"><img src="images/demo/gallery/7.png"><img src="images/demo/gallery/8.png"><img src="images/demo/gallery/9.png"><img src="images/demo/gallery/10.png"></marquee>
+ <script type="text/javascript">
+  document.write(droplists());
+  document.write(createMonth());
+  </script>
+ </div>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+
+
+<div class="wrapper row3">
+  <div class="rounded">
+    <main class="container clear"> 
+
+    
+      <!-- main body --> 
+      <!-- ################################################################################################ -->
+     
+      
+      <!-- ################################################################################################ --> 
+      <!-- ################################################################################################ -->
+      <div class="group">
+        <h2>Quickly Find What You Are Looking For</h2>
+        <div class="one_quarter  "> 
+          <!-- ####################################################################################### -->
+
+          <ul >
+            <li><a href="#">Academic Advisory</a></li>
+            <li><a href="#">Academic Assistance</a></li>
+            <li><a href="#">Academic Calendars</a></li>
+            <li><a href="#">Academics Office</a></li>
+            <li><a href="#">Administration</a></li>
+            <li><a href="#">Adult Learners</a></li>
+            <li><a href="#">Alumni Chapters</a></li>
+            <li><a href="#">Alumni Events</a></li>
+            <li><a href="#">Athletics</a></li>
+            <li><a href="#">Campus Life At a Glance</a></li>
+            <li><a href="#">Campus Recreation</a></li>
+            <li><a href="#">Campus Safety &amp; Security</a></li>
+          </ul>
+          <!-- ################################################################################################ --> 
+        </div>
+        <div class="one_quarter "> 
+          <!-- ################################################################################################ -->
+          <ul >
+            <li><a href="#">Class Schedules</a></li>
+            <li><a href="#">Counselling Center</a></li>
+            <li><a href="#">Course Descriptions &amp; Catalogue</a></li>
+            <li><a href="#">Department Directory</a></li>
+            <li><a href="#">Departments &amp; Programs</a></li>
+            <li><a href="#">Fellowships</a></li>
+            <li><a href="#">Finals Schedules</a></li>
+            <li><a href="#">Financial Aid</a></li>
+            <li><a href="#">Fitness and Recreation Facilities</a></li>
+            <li><a href="#">Global Learning</a></li>
+            <li><a href="#">Graduate</a></li>
+            <li><a href="#">Graduate Admissions</a></li>
+          </ul>
+          <!-- ################################################################################################ --> 
+        </div>
+        
+        <div class="one_quarter "> 
+          <!-- ################################################################################################ -->
+          <ul >
+            <li><a href="#">Graduate Health Services</a></li>
+            <li><a href="#">Graduate Housing</a></li>
+            <li><a href="#">Graduate Programs</a></li>
+            <li><a href="#">Graduate Student Association</a></li>
+            <li><a href="#">Graduate Studies</a></li>
+            <li><a href="#">Honours Program</a></li>
+            <li><a href="#">Interactive Schedule</a></li>
+            <li><a href="#">International Programs</a></li>
+            <li><a href="#">International Students</a></li>
+            <li><a href="#">Intramural Sports</a></li>
+            <li><a href="#">Language Resources</a></li>
+            <li><a href="#">Maps and Directions</a></li>
+          </ul>
+          <!-- ################################################################################################ --> 
+        </div>
+       
+      </div>
+      <!-- ################################################################################################ --> 
+      <!-- / main body -->
+      
+    </main>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+  <div class="rounded">
+    <footer id="footer" class="clear"> 
+      <!-- ################################################################################################ -->
+      <div class="one_third first">
+        <figure class="center"><img class="btmspace-15" src="images/demo/worldmap.png" alt="">
+           
+          <figcaption><a href="https://www.google.co.in/maps/place/Poornima+Institute+of+Engineering+and+Technology">Find Us With Google Maps &raquo;</a></figcaption>
+        </figure>
+      </div>
+      <div class="one_third">
+        <address>
+       Address: ISI - 2 RIICO Institutional Area,<br>
+        Poornima Marg, Sitapura,<br>
+         Jaipur, Rajasthan <br>
+         302022
+        <br>
+        <i class="fa fa-phone pright-10"></i>  099285 55222<br>
+        <i class="fa fa-envelope-o pright-10"></i> <a href="http://www.poornima.org">poornima.org</a>
+        </address>
+      </div>
+      <div class="one_third">
+        <p class="nospace btmspace-10">we are active</p>
+        <ul class="faico clear">
+          <li><a class="faicon-twitter" href="https://twitter.com/Poornima_Jaipur//"><i class="fa fa-twitter"></i></a></li>
+          <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+          <li><a class="faicon-facebook" href="https://www.facebook.com/poornimagroup"><i class="fa fa-facebook"></i></a></li>
+          
+        </ul>
+        <form class="clear" method="post" action="#">
+          <fieldset>
+            <legend>Subscribe To Our Newsletter:</legend>
+            <input type="text" value="" placeholder="Enter Email Here&hellip;">
+            <button class="fa fa-sign-in" type="submit" title="Sign Up"><em>Sign Up</em></button>
+          </fieldset>
+        </form>
+      </div>
+      <!-- ################################################################################################ --> 
+    </footer>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2017 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    
+    <!-- ################################################################################################ --> 
+  </div>
+</div>
+<!-- JAVASCRIPTS --> 
+<script src="layout/scripts/jquery.min.js"></script> 
+<script src="layout/scripts/jquery.fitvids.min.js"></script> 
+<script src="layout/scripts/jquery.mobilemenu.js"></script> 
+<script src="layout/scripts/tabslet/jquery.tabslet.min.js"></script>
+<script src="assets/js/jquery.js"></script>
+   
+    <!-- SLIDER SCRIPTS LIBRARY -->
+    <script src="assets/Slides-SlidesJS-3/examples/playing/js/jquery.slides.min.js"></script>
+    
+    <!-- CUSTOM SCRIPT-->
+    <script>
+        $(document).ready(function () {
+            $('#slides').slidesjs({
+                width: 940,
+                height: 528,
+                play: {
+                    active: true,
+                    auto: true,
+                    interval: 4000,
+                    swap: true
+                }
+            });
+        });
+
+    </script>
+</body>
+</html>
