bd83e0621b8c56b5c5fb39b23c8218002098f78b
gaurav7goyal <gaurav1997dlp@gmail.com>
2017-03-31 00:43:43 +0530

    new

--- PDS/recruiters/upcoming.php
+++ PDS/recruiters/upcoming.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-	<title>PDF</title>
+	<title>PDS</title>
 
 
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,800' rel='stylesheet' type='text/css'>
@@ -43,7 +43,7 @@
 					<div id="main-header" class="main-header header-sticky">
 						<div class="inner-header clearfix">
 							<div class="logo">
-								<a href="../index.php">PDF</a>
+								<a href="../index.php">PDS</a>
 							</div>
 							<div class="header-right-toggle pull-right hidden-md hidden-lg">
 								<a href="javascript:void(0)" class="side-menu-button"><i class="fa fa-bars"></i></a>
